a277a4beb7f1ac2871324bbc03911e350d5d1d09
Trukhanov Artem <trukhanovartem@me.com>
2017-04-10 14:53:24 +0300

    Remove unimplemented sharings

--- src/ShareVia.js
+++ src/ShareVia.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 
 class ShareVia extends Component {
-  slack = () => {
-    window.open(encodeURI(`slack://open?team=mygroupartem&id=general&message=${this.props.text}`), '_self');
-  };
-
   whatsapp = () => {
     window.open(encodeURI(`whatsapp://send?text=${this.props.text}`), '_self');
   };
@@ -14,16 +10,10 @@ class ShareVia extends Component {
     window.open(`mailto:?Subject=Palindrome&body=${this.props.text}`, '_self')
   };
 
-  jabber = () => {
-    window.open(encodeURI(`xmpp:romeo@montague.net?message;body=${this.props.text}`), '_self')
-  };
-
   render() {
     return (
       <div className='Share-entered-item'>
-        <FontAwesome name='slack' className={'clickable'} onClick={this.slack} />
         <FontAwesome name='whatsapp' className={'clickable'} onClick={this.whatsapp} />
-        <FontAwesome name='comment' className={'clickable'} onClick={this.jabber} />
         <FontAwesome name='envelope' className={'clickable'} onClick={this.email} />
       </div>
     );
